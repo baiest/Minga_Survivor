c99bada0220f559816a74a6d9351b8bdebff00cd
= <=>
2020-10-23 16:42:45 -0500

    añadi un nodo player y otro floor para probar el movimiento del player

---
+++ scripts/script_player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var Gravedad = 2500
+var Velocidad = Vector2()
+var run_speed = 350
+var jump_speed = -500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func get_imput():
+	Velocidad.x = 0
+	var rigth = Input.is_action_pressed("ui_right")
+	var left = Input.is_action_pressed("ui_left")
+	var jump = Input.is_action_pressed("ui_up")
+	
+	if is_on_floor() and jump:
+		Velocidad.y = jump_speed
+	if rigth:
+		Velocidad.x += run_speed
+	if left:
+		Velocidad.x -= run_speed
+
+func _physics_process(delta):
+	Velocidad.y += Gravedad * delta
+	get_imput()
+	Velocidad = move_and_slide(Velocidad, Vector2(0,-1))
+	
+
+	
+
